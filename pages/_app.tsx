354769cfe5ab295cff3b0c085db5e482b840a97d
Rokas <rookas.rudzenskas@gmail.com>
2022-11-24 18:19:18 +0200

    feat: add auth
    - Add auth to the project
    - Add a commit message

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import {ThirdwebProvider} from "@thirdweb-dev/react/solana";
 import { Network } from '@thirdweb-dev/sdk/solana';
 import {PhantomWalletAdapter} from "@solana/wallet-adapter-phantom";
+import {WalletProvider} from "@solana/wallet-adapter-react";
 
 export const network: Network = 'devnet';
 export const domain = "example.com";
@@ -17,7 +18,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         }}
         network={network}
       >
-        <Component {...pageProps} />
+        <WalletProvider wallets={[wallet]}>
+            <Component {...pageProps} />
+        </WalletProvider>
       </ThirdwebProvider>
   )
 }

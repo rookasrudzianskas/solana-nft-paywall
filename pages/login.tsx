21bf6f2c1584e5e9a4ed56e845cea3b273407011
Rokas <rookas.rudzenskas@gmail.com>
2022-11-24 18:20:53 +0200

    feat: add login page
    - add login page
    - add login page styles

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LoginPage = ({}) => {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center text-center bg-[#F5AB0B]">
+
+        </div>
+    );
+};
+
+export default LoginPage;
+// by Rokas with ❤️

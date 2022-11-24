03fefae374285ec8a6a76109bb632725bdfa29d2
Rokas <rookas.rudzenskas@gmail.com>
2022-11-24 18:14:26 +0200

    feat: add thirdweb provider
    This commit adds the thirdweb provider to the app.
    
    It also adds the network and wallet to the app.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {ThirdwebProvider} from "@thirdweb-dev/react/solana";
+import { Network } from '@thirdweb-dev/sdk/solana';
+import {PhantomWalletAdapter} from "@solana/wallet-adapter-phantom";
 
+export const network: Network = 'devnet';
+export const domain = "example.com";
+export const wallet = new PhantomWalletAdapter();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-      <ThirdwebProvider>
+      <ThirdwebProvider
+        authConfig={{
+            authUrl: "/api/auth",
+            domain: process.env.VERCEL_URL || domain,
+            loginRedirect: "/",
+        }}
+        network={network}
+      >
         <Component {...pageProps} />
       </ThirdwebProvider>
   )
